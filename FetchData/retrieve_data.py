f8ca9907d305c095be885f210659998240c7a695
Trix <trbelleza@up.edu.ph>
2018-12-26 19:38:30 +0800

    Getting the output predictions

---
+++ FetchData/retrieve_data.py
@@ -0,0 +1,95 @@
+import pymysql
+import pandas as pd
+import nltk
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MultiLabelBinarizer
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# nltk.download('stopwords')
+
+stop_words =  nltk.corpus.stopwords.words('english')
+new_stop_words = ['(', ')', '[', ']', '{', '}', '"', "'", '``', '""',"''", ',', '.', '“', '”', '’', '`']
+stop_words.extend(new_stop_words)
+
+# # Connect to the database
+db_connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='592008',
+                             db='questions_db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+# try:
+#     with connection.cursor() as cursor:
+#         # Read a single record
+#         sql = "SELECT `tags` FROM `Questions`"
+#         cursor.execute(sql)
+#         result = cursor.fetchall()
+#         print(result)
+# finally:
+
+#     connection.close()
+categories = ['python', 'javascript', 'java', 'c', 'r', 'while_loop', 'for_loop']
+df = pd.read_sql('SELECT * FROM Train_data', con=db_connection)
+train, test = train_test_split(df, random_state=42, test_size=0.20, shuffle=True)
+
+train_text = train['question_body']
+test_text = test['question_body']
+
+vectorizer = TfidfVectorizer(strip_accents='unicode', stop_words=stop_words, analyzer='word', ngram_range=(1,3), norm='l2')
+vectorizer.fit(train_text)
+vectorizer.fit(test_text)
+
+x_train = vectorizer.transform(train_text)
+y_train = train.drop(labels = ['question_body'], axis=1)
+
+x_test = vectorizer.transform(test_text)
+y_test = test.drop(labels = ['question_body'], axis=1)
+
+#print("TEST")
+# print(test)
+
+# Using pipeline for applying linearSVC and one vs rest classifier
+SVC_pipeline = Pipeline([
+                ('clf', OneVsRestClassifier(LinearSVC(), n_jobs=1)),
+            ])
+
+predicted_list = []
+
+for category in categories:
+    print('... Processing {}'.format(category))
+    
+    # train the model using X_dtm & y
+
+    SVC_pipeline.fit(x_train, train[category])
+    # print("SVC Classes")
+    # print(SVC_pipeline.classes_)
+    # compute the testing accuracy
+    prediction = SVC_pipeline.predict(x_test)
+    print("prediction:")
+    print(prediction)
+    print('Test accuracy is {}'.format(accuracy_score(test[category], prediction)))
+
+    predicted = {'question_body' : '', 'predicted_tags' : []}
+    # print(test["question_body"].values[1])
+    predicted['predicted_tags'].append(category)
+    
+    for i in range(len(prediction)):
+        if prediction[i] == 1:
+            predicted['question_body'] = test["question_body"].values[i]
+    predicted_list.append(predicted)
+
+print("Predicted list [1]:")
+print(predicted_list[1])    
+    # print("End")

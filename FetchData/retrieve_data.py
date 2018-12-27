ca3050b2fdf5f809da6abf1cb7939740730097da
Trix <trbelleza@up.edu.ph>
2018-12-27 09:33:42 +0800

    Added data preprocessing

--- FetchData/retrieve_data.py
+++ FetchData/retrieve_data.py
@@ -30,18 +30,10 @@ db_connection = pymysql.connect(host='localhost',
                              db='questions_db',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-# try:
-#     with connection.cursor() as cursor:
-#         # Read a single record
-#         sql = "SELECT `tags` FROM `Questions`"
-#         cursor.execute(sql)
-#         result = cursor.fetchall()
-#         print(result)
-# finally:
-
-#     connection.close()
 categories = ['python', 'javascript', 'java', 'c', 'r', 'while_loop', 'for_loop']
 df = pd.read_sql('SELECT * FROM Train_data', con=db_connection)
+db_connection.close()
+
 train, test = train_test_split(df, random_state=42, test_size=0.20, shuffle=True)
 
 train_text = train['question_body']
@@ -57,9 +49,6 @@ y_train = train.drop(labels = ['question_body'], axis=1)
 x_test = vectorizer.transform(test_text)
 y_test = test.drop(labels = ['question_body'], axis=1)
 
-#print("TEST")
-# print(test)
-
 # Using pipeline for applying linearSVC and one vs rest classifier
 SVC_pipeline = Pipeline([
                 ('clf', OneVsRestClassifier(LinearSVC(), n_jobs=1)),
@@ -67,29 +56,26 @@ SVC_pipeline = Pipeline([
 
 predicted_list = []
 
+for i in range(len(test)):
+    predicted = {'question_body' : '', 'predicted_tags' : []}
+    predicted['question_body'] = test["question_body"].values[i]
+    predicted_list.append(predicted)
+
 for category in categories:
     print('... Processing {}'.format(category))
     
     # train the model using X_dtm & y
-
     SVC_pipeline.fit(x_train, train[category])
-    # print("SVC Classes")
-    # print(SVC_pipeline.classes_)
+    
     # compute the testing accuracy
     prediction = SVC_pipeline.predict(x_test)
     print("prediction:")
     print(prediction)
     print('Test accuracy is {}'.format(accuracy_score(test[category], prediction)))
 
-    predicted = {'question_body' : '', 'predicted_tags' : []}
-    # print(test["question_body"].values[1])
-    predicted['predicted_tags'].append(category)
-    
     for i in range(len(prediction)):
         if prediction[i] == 1:
-            predicted['question_body'] = test["question_body"].values[i]
-    predicted_list.append(predicted)
+            predicted_list[i]["predicted_tags"].append(category)
 
 print("Predicted list [1]:")
 print(predicted_list[1])    
-    # print("End")

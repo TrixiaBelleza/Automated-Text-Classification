f6b1f29b573e99d4d51fa3b519c24f6ca6cb23b7
Trix <trbelleza@up.edu.ph>
2019-01-03 21:10:53 +0800

    Fetches dynamic input question on StackOverflow Website

--- ext-SO/contentScript.js
+++ ext-SO/contentScript.js
@@ -1,33 +1,19 @@
-// var threshold = 50;
-
-// chrome.storage.sync.get({
-// 	threshold:'50'
-// }, function(items) {
-// 	threshold = items.threshold;
-// });
-
-//Deal with newly loaded tweets
-// function DOMModificationHandler(){
-// 	$(this).unbind('DOMSubtreeModified.event1');
-// 	setTimeout(function() {
-// 		modify();
-// 		$('#timeline').bind('DOMSubtreeModified.event1', DOMModificationHandler);
-// 	}, 10);
-// }
-// $('#timeline').bind('DOMSubtreeModified.event1', DOMModificationHandler);
-
 //find and modify tall tweets
-$('#wmd-input').each(function(index){
-	var t = $(this);
-	console.log(t.text())
-	// var len = t.split(/\r\n|\r|\n/).length;
 
-	// if(!$(this).hasClass("squished") && len > threshold){
-	// 	$(this).addClass("squished");
-	// 	$(this).html(`<button class="squish-button EdgeButton EdgeButton--primary" data-original-content="${encodeURI(t)}">Show Long Tweet</button>`);
-		
-	// 	//Add listeners
-	// 	chrome.runtime.sendMessage({message:"listeners"}, function(response) {
-	// 	});
-	// }
+$('#wmd-input').on("input", function(){
+	var t = $(this).val();
+	console.log(t)
 });
+
+var tagrecom_div = document.createElement("div");
+tagrecom_div.id = "tagrecom_div"
+tagrecom_div.class = "tagrecom_div"
+document.getElementById("tag-editor").appendChild(tagrecom_div);
+
+var tagrecom_btn = document.createElement('button');
+tagrecom_btn.id = "tagrecom_btn";
+var tagrecom_btn_txt = document.createTextNode("Tag Suggestions");  
+tagrecom_btn.appendChild(tagrecom_btn_txt); 
+document.getElementById('tagrecom_div').appendChild(tagrecom_btn)
+
+

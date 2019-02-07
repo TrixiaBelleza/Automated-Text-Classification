e30a073c309d6a06c209702e8fa2fad85aad00cd
Trix <trbelleza@up.edu.ph>
2019-02-07 09:18:15 +0800

    Adds tags directly to tags section, similarly to the actual

--- ext-SO/contentScript.js
+++ ext-SO/contentScript.js
@@ -48,7 +48,24 @@ function openModal() {
     	  tagspan.classList.add('s-tag', 'rendered-element');
     	  tagspan.appendChild(document.createTextNode("javascript"));
     	  document.getElementsByClassName('tag-editor s-input')[0].firstChild.appendChild(tagspan);
-    	  // document.getElementById('tageditor-replacing-tagnames--input').value = "javascript";
+
+    	  var removeTag = document.createElement('a');
+    	  removeTag.classList.add('js-delete-tag', 's-tag--dismiss');
+    	  removeTag.setAttribute("title", "Remove tag");
+    	  document.getElementsByClassName('s-tag rendered-element')[0].appendChild(removeTag);
+
+    	  var removeTagSVG = document.createElement('svg');
+    	  removeTagSVG.style.pointerEvents = 'none';
+    	  removeTagSVG.classList.add('svg-icon', 'iconClearSm');
+    	  removeTagSVG.setAttribute("height", "12");
+    	  removeTagSVG.setAttribute("width", "12"); 
+    	  removeTagSVG.setAttribute("viewBox", "0 0 14 14");
+    	  removeTag.appendChild(removeTagSVG);
+
+    	  var pathSVG = document.createElement('path');
+    	  pathSVG.setAttribute("d", "M12 3.41L10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7z");
+    	  removeTagSVG.appendChild(pathSVG);
+
 		  loader.style.display = 'none';
 	    },
 	    error: function(e, s, t) {
